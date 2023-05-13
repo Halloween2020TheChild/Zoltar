6cfb0be498693a20eb8b6e662ef56be48f3b096b
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-13 17:49:33 -0400

    working OkHTTP implementation

--- ZoltarController.groovy
+++ ZoltarController.groovy
@@ -1,9 +1,9 @@
-// https://mvnrepository.com/artifact/tangrammer/java-net-http-client
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets
+
 //import java.net.http.HttpClient;
 //import java.net.http.HttpRequest;
 //import java.net.http.HttpRequest.BodyPublishers;
@@ -16,7 +16,10 @@ import java.security.cert.X509Certificate
 
 import javax.net.ssl.SSLContext
 import org.apache.commons.io.IOUtils
+import org.apache.http.HttpEntity
 import org.apache.http.HttpRequest
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpPost
@@ -28,6 +31,7 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager
 import org.apache.http.ssl.SSLContexts
@@ -36,36 +40,43 @@ import org.apache.http.util.EntityUtils
 
 import com.neuronrobotics.bowlerstudio.BowlerStudio
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.squareup.okhttp.MediaType
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.RequestBody
+import com.squareup.okhttp.Response
+
 
 public class GPTInterface {
 
 	public final String AI_MODEL_NAME = "davinci";
 
-	private String APIKey;
+	private String API_KEY;
+	private static final String CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions";
 
-
-	final TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
-		@Override
-		public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-			return true;
-		}
-
-	};
-	final SSLContext sslContext = SSLContexts.custom()
-	.loadTrustMaterial(null, acceptingTrustStrategy)
-	.build();
-	final SSLConnectionSocketFactory sslsf =
-	new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-	final Registry<ConnectionSocketFactory> socketFactoryRegistry =
-	RegistryBuilder.<ConnectionSocketFactory> create()
-	.register("https", sslsf)
-	.register("http", new PlainConnectionSocketFactory())
-	.build();
-
-	final BasicHttpClientConnectionManager connectionManager =
-	new BasicHttpClientConnectionManager(socketFactoryRegistry);
+	//
+	//	final TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+	//		@Override
+	//		public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+	//			return true;
+	//		}
+	//
+	//	};
+	//	final SSLContext sslContext = SSLContexts.custom()
+	//	.loadTrustMaterial(null, acceptingTrustStrategy)
+	//	.build();
+	//	final SSLConnectionSocketFactory sslsf =
+	//	new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+	//	final Registry<ConnectionSocketFactory> socketFactoryRegistry =
+	//	RegistryBuilder.<ConnectionSocketFactory> create()
+	//	.register("https", sslsf)
+	//	.register("http", new PlainConnectionSocketFactory())
+	//	.build();
+	//
+	//	final BasicHttpClientConnectionManager connectionManager =
+	//	new BasicHttpClientConnectionManager(socketFactoryRegistry);
 	public GPTInterface(String APIKey) {
-		this.APIKey = APIKey;
+		this.API_KEY = APIKey;
 	}
 
 	public String request(String phrase) throws IOException {
@@ -73,31 +84,27 @@ public class GPTInterface {
 	}
 
 	public String request(String phrase, float randomness) throws IOException {
-
-		//generate JSON request body
 		String requestBody = String.format("{\"model\":\"%s\",\"prompt\":\"%s\",\"temperature\":%f}", AI_MODEL_NAME, phrase, randomness);
-		String payload = "{\"prompt\": \"Hello, ChatGPT!\"}";
 		
-		HttpPost post = new HttpPost("http://api.openai.com/v1/completions");
-		post.setEntity(new StringEntity(payload));
-		post.setHeader("content-type", "application/json")
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Authorization", String.format("Bearer %s", APIKey))
-		println post.getAllHeaders()
-		try  {
-			CloseableHttpClient httpClient = HttpClients.custom()
-					.setConnectionManager(connectionManager)
-					.build();
-			CloseableHttpResponse response = httpClient.execute(post);
-			println response
-			InputStream responseGetEntityGetContent = response.getEntity().getContent();
-			return IOUtils.toString(responseGetEntityGetContent, StandardCharsets.UTF_8.name());;
-		}catch(Throwable t) {
-			BowlerStudio.printStackTrace(t)
-			return "";
+		OkHttpClient client = new OkHttpClient()
+
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType, requestBody);
+
+		Request request = new Request.Builder()
+				.url(CHATGPT_API_URL)
+				.method("POST", body)
+				.addHeader("Content-Type", "application/json")
+				.addHeader("Authorization", "Bearer " + API_KEY)
+				.build();
+
+		try {
+			Response response = client.newCall(request).execute();
+			return response.body().string();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		return "";
 	}
 }
 String keyLocation = ScriptingEngine.getWorkspace().getAbsolutePath()+"/gpt-key.txt"
