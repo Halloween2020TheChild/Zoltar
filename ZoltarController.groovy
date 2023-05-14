801260ed75c7c925a83d4ebe68a4816da6f6b7aa
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-14 10:10:38 -0400

    semi randon attitude

--- ZoltarController.groovy
+++ ZoltarController.groovy
@@ -7,8 +7,7 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
 
-
-
+import com.neuronrobotics.bowlerstudio.BowlerKernel
 import com.neuronrobotics.bowlerstudio.BowlerStudio
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
 import com.squareup.okhttp.MediaType
@@ -29,6 +28,8 @@ public class GPTInterface {
 	private static final String CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions";
 	Type TT_mapStringString = new TypeToken<HashMap<String, Object>>() {}.getType();
 	Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+	int maxSize = 240
+	
 	//
 	//	final TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
 	//		@Override
@@ -65,6 +66,11 @@ public class GPTInterface {
    }'
 	 */
 	public String request(String phrase, float randomness) throws IOException {
+		if(Math.random()>0.5)
+			phrase="Pretend you are an inspirational Fortuine teller. Keep your response less than "+(maxSize*0.5)+" charectors. As a Fortuine teller respond to: "+phrase
+		else
+			phrase="Pretend you are an mean joke telling Fortuine teller. Keep your response less than "+(maxSize*0.5)+" charectors. As a Fortuine teller make a joke response to: "+phrase
+			
 		String requestBody = String.format("{\"model\":\"%s\",\"messages\":\"%s\",\"temperature\":%f}", AI_MODEL_NAME, phrase, randomness);
 		HashMap<String,Object> message = new HashMap(); 
 		HashMap<String,String> messages = new HashMap();
@@ -89,10 +95,19 @@ public class GPTInterface {
 				.addHeader("Content-Type", "application/json")
 				.addHeader("Authorization", "Bearer " + API_KEY)
 				.build();
-
+		
 		try {
 			Response response = client.newCall(request).execute();
-			return response.body().string();
+			String jsonString = response.body().string();
+			HashMap<String, Object> database = gson.fromJson(jsonString, TT_mapStringString);
+			ArrayList<Object> choices = database.get("choices")
+			HashMap<String, Object> firstChoice = choices.get(0)
+			HashMap<String, Object> messageContent = firstChoice.get("message")
+			String ret = messageContent.get("content").toString()
+			println ret
+			if(ret.length()>maxSize)
+				ret=ret.substring(0, maxSize)
+			return ret
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -103,9 +118,9 @@ println "Loading API key from "+keyLocation
 String content = new String(Files.readAllBytes(Paths.get(keyLocation)));
 println "API key: "+content
 GPTInterface gpt = new GPTInterface(content)
-String response  = gpt.request("Pretend you are a Zoltar Machine. Keep your response less than 120 charectors. As a Zoltar Machinerespond to: What is my fortune?")
+String response  = gpt.request("What is my fortune?",1)
 println "\n\nResponse\n"+response
-//BowlerStudio.speak(response)
+BowlerKernel.speak(response, 150, 0, 100, 1.0, 1.0)
 
 
 
