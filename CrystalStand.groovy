8bd7d72a5ecd48a12ca2e93b1197485b6bc49bf3
JansenSmith <jansen@bstarengineering.com>
2023-07-24 15:56:05 -0400

    rounded

--- CrystalStand.groovy
+++ CrystalStand.groovy
@@ -1,5 +1,20 @@
 // code here
 
 // make a GENTLY rounded cylinder
+
+def radius = 3 + 3/8 // 3 + 3/8 inches
+radius = radius * 25.4 // conv to mm
+
+CSG roundedCylinder = new RoundedCylinder(radius,60.0)
+                                .cornerRadius(10)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+CSG body = roundedCylinder
+
+// make an identical non-rounded cylinder but half as tall
+// union
 // diff w a Wedge or Isosceles
 // diff w an internal cylinder
+
+
+
+return body
