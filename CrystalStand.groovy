5a36dfdfb5f15c3f4a78c8d06cd2632472898c92
JansenSmith <jansen@bstarengineering.com>
2023-07-24 18:49:47 -0400

    curved to my liking

--- CrystalStand.groovy
+++ CrystalStand.groovy
@@ -2,23 +2,24 @@
 
 def radius = 3 + 3/8 // 3 + 3/8 inches
 radius = radius * 25.4 // conv to mm
-def height = 60 // 6mm
+def height = 6 // 6mm
 def radius_inner = 3 + 3/8 // 3 + 3/8 inches
 radius_inner = radius_inner * 25.4 // conv to mm
 def cutout_degree = 120
 
 // make a gently rounded cylinder
 CSG top = new RoundedCylinder(radius,height)
-                                .cornerRadius(10)// sets the radius of the corner
+                                .cornerRadius(1.5)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
 
-// make a subtly rounded cylinder
-CSG bottom = new RoundedCylinder(radius,height/2)
-                                .cornerRadius(2)// sets the radius of the corner
-                                .toCSG()// converts it to a CSG tor display
+//// make a subtly rounded cylinder
+//CSG bottom = new RoundedCylinder(radius,height/2)
+//                                .cornerRadius(1)// sets the radius of the corner
+//                                .toCSG()// converts it to a CSG tor display
 
 // union the two
-CSG body = top.union(bottom)
+//CSG body = top.union(bottom)
+CSG body = top
 
 // diff w a Wedge or Isosceles
 //CSG cutout = new Isosceles(60,25,40).toCSG()
