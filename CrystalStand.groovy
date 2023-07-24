ba55f47cb6ef093095661bd2db18a08f1e32dbfa
JansenSmith <jansen@bstarengineering.com>
2023-07-24 16:00:18 -0400

    smoothed

--- CrystalStand.groovy
+++ CrystalStand.groovy
@@ -4,11 +4,17 @@
 
 def radius = 3 + 3/8 // 3 + 3/8 inches
 radius = radius * 25.4 // conv to mm
+def height = 2 + 7/8
+height = height * 25.4
 
-CSG roundedCylinder = new RoundedCylinder(radius,60.0)
+CSG top = new RoundedCylinder(radius,height)
                                 .cornerRadius(10)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
-CSG body = roundedCylinder
+
+CSG bottom = new RoundedCylinder(radius,height/2)
+                                .cornerRadius(2)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+CSG body = top.union(bottom)
 
 // make an identical non-rounded cylinder but half as tall
 // union
